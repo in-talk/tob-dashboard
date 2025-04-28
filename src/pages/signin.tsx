4ed60101b0165284cef67a1be34eabccd8bace06
Muhammad Abdullah <abdullah.spgc3424@outlook.com>
2025-04-28 11:05:38 +0500

    Feat:Added password show hide functionality in sign in page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { EyeOpenIcon, EyeClosedIcon } from "@radix-ui/react-icons";
+
 import Image from "next/image";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
 
@@ -45,30 +48,46 @@ export default function SignIn() {
               {error && (
                 <p className="text-red-500 text-center mb-2">{error}</p>
               )}
-               <form onSubmit={handleSignIn} className="space-y-4">
-              <div className="mx-auto max-w-xs">
-                <input
-                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                  type="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-                <input
-                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                  type="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-                <button type="submit" className="mt-5 tracking-wide font-semibold bg-[#3b65f5] text-gray-100 w-full py-4 rounded-lg  transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                  <Image src='/login.svg' alt="login" width={24} height={24} />
-                  <span className="ml-3">Sign In</span>
-                </button>
-                
-              </div>
+              <form onSubmit={handleSignIn} className="space-y-4">
+                <div className="mx-auto max-w-xs">
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                  <div className="relative">
+                    <input
+                      className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                      type={showPassword ? "text" : "password"} // toggle between text and password
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                    <button
+                      type="button"
+                      className="absolute inset-y-0 right-0 mt-5 flex items-center pr-3"
+                      onClick={() => setShowPassword(!showPassword)} // toggle password visibility
+                    >
+                      {showPassword ? <EyeOpenIcon />: <EyeClosedIcon /> }
+                    </button>
+                  </div>
+                  <button
+                    type="submit"
+                    className="mt-5 tracking-wide font-semibold bg-[#3b65f5] text-gray-100 w-full py-4 rounded-lg  transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                  >
+                    <Image
+                      src="/login.svg"
+                      alt="login"
+                      width={24}
+                      height={24}
+                    />
+                    <span className="ml-3">Sign In</span>
+                  </button>
+                </div>
               </form>
             </div>
           </div>
